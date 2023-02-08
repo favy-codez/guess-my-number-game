4e4abc133c16d6a753c9cba2927ee94a4574e4d8
favy-codez <107569845+favy-codez@users.noreply.github.com>
2023-02-08 03:40:29 +0100

    converted the input value to  numbe~

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 'use strict';
-document.querySelector('.number') = number;
-document.querySelector('.message') = message;
-document.querySelector('.guess') = guessBox;
-document.querySelector('.btn') = guessBtn;
-document.querySelector('.label-score') = score;
-document.querySelector('highscore') = highscore;
+document.querySelector('.number').textContent = 3;
+document.querySelector('.message').textContent = 'correct number! 🎉';
+document.querySelector('.guess').value = 33;
+// document.querySelector('.btn') = guessBtn;
+// document.querySelector('.label-score') = score;
+// document.querySelector('highscore') = highscore;
+
+// convert the value of the input to number
+const guess = Number(document.querySelector('.guess').value);
+console.log(guess, typeof guess);
