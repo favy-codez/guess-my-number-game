87151eca11118b9c7d20adc2d1083f5e890cb2b6
favy-codez <107569845+favy-codez@users.noreply.github.com>
2023-02-08 03:25:42 +0100

    selected elements for DOM manipulation

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 'use strict';
+document.querySelector('.number') = number;
+document.querySelector('.message') = message;
+document.querySelector('.guess') = guessBox;
+document.querySelector('.btn') = guessBtn;
+document.querySelector('.label-score') = score;
+document.querySelector('highscore') = highscore;
