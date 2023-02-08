4d7ca0802b44f57630ef35b3a5d7e0083e797e6e
favy-codez <107569845+favy-codez@users.noreply.github.com>
2023-02-08 03:58:52 +0100

    created if-else statement~

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 'use strict';
-document.querySelector('.number').textContent = 3;
+
 document.querySelector('.message').textContent = 'correct number! 🎉';
 document.querySelector('.guess').value = 33;
-// document.querySelector('.btn') = guessBtn;
 // document.querySelector('.label-score') = score;
 // document.querySelector('highscore') = highscore;
 
-// convert the value of the input to number
-const guess = Number(document.querySelector('.guess').value);
-console.log(guess, typeof guess);
+
+//  add event lister to the check button
+document.querySelector('.check').addEventListener('click', function() {
+    // convert the value of the input to number
+    const guess = Number(document.querySelector('.guess').value);
+})
+
+// target secret number
+const secretNum = Math.floor(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNum ;
+
+
+// create an if else statement
+if(!guess) {
+    document.querySelector('.message').textContent = 'No number! 😡';
+}else if (guess === secretNum){  
+    document.querySelector('.message').textContent = 'correct number! 🎉';
+}else if (guess > secretNum){  
+    document.querySelector('.message').textContent = 'Too high 📈';
+}else if (guess === secretNum){  
+    document.querySelector('.message').textContent = 'Too high 📉';
+}
